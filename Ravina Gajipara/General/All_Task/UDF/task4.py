ba06ac5ba4704818ec08740ac3536dd0eaca2406
Ravina Gajipara <161796519+RavinaGajipara@users.noreply.github.com>
2026-02-25 00:13:52 -0800

    Refactor input handling for list calculations
    Refactor user input handling for list calculations to support different numbers of lists more clearly. Consolidate input logic for one, two, and three lists, while maintaining functionality for more than three lists.

--- Ravina Gajipara/General/All_Task/UDF/task4.py
+++ Ravina Gajipara/General/All_Task/UDF/task4.py
@@ -35,21 +35,70 @@ def calculate(*lists):
         print("Odd List:", odd_list)
 
 
-# 🔹 User Input Section
+# User Input Section
 
 num_lists = int(input("Enter number of lists: "))
 
-all_lists = []
+if num_lists == 1:
+    n = int(input(f"Enter number of elements for list 1: "))
+    ls1 = []
 
-for i in range(1, num_lists + 1):
-    n = int(input(f"Enter number of elements for list {i}: "))
-    
-    temp_list = []
-    for j in range(n):
-        element = int(input(f"Enter element {j+1}: "))
-        temp_list.append(element)
-    
-    all_lists.append(temp_list)
+    for i in range(n):
+        element = int(input(f"Enter element {i+1}: "))
+        ls1.append(element)
 
-# Call function
-calculate(*all_lists)
+    calculate(ls1)
+
+elif num_lists == 2:
+    ls1 = []
+    ls2 = []
+
+    n1 = int(input(f"Enter number of elements for list 1: "))
+    for i in range(n1):
+        element = int(input(f"Enter element {i+1}: "))
+        ls1.append(element)
+
+    n2 = int(input(f"Enter number of elements for list 2: "))
+    for i in range(n2):
+        element = int(input(f"Enter element {i+1}: "))
+        ls2.append(element)
+
+    calculate(ls1, ls2)
+
+elif num_lists == 3:
+    ls1 = []
+    ls2 = []
+    ls3 = []
+
+    n1 = int(input(f"Enter number of elements for list 1: "))
+    for i in range(n1):
+        element = int(input(f"Enter element {i+1}: "))
+        ls1.append(element)
+
+    n2 = int(input(f"Enter number of elements for list 2: "))
+    for i in range(n2): 
+        element = int(input(f"Enter element {i+1}: "))
+        ls2.append(element)
+
+    n3 = int(input(f"Enter number of elements for list 3: "))
+    for i in range(n3):
+        element = int(input(f"Enter element {i+1}: "))
+        ls3.append(element)
+
+    calculate(ls1, ls2, ls3)
+
+else:
+    all_lists = []
+
+    for i in range(1, num_lists + 1):
+        n = int(input(f"Enter number of elements for list {i}: "))
+    
+        temp_list = []
+        for j in range(n):
+            element = int(input(f"Enter element {j+1}: "))
+            temp_list.append(element)
+    
+        all_lists.append(temp_list)
+        
+        calculate(*all_lists)
+
